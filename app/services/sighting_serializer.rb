b1a8c0b2dbdede9fb3b7e457308a3515af705a28
Drew Thompson <drewallynthompson@gmail.com>
2021-01-09 11:13:13 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,9 +5,8 @@ class SightingSerializer
   
   def to_serialized_json
     @sighting.to_json(:include => {
-      :bird => {:only => {
-        {
-      }}
-    })
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+      }, :except => [:updated_at])
   end
 end
